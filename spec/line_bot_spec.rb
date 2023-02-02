706642ec4bb47fd194b3bd0e19613a43992035fe
Yusuke Ishida <37574368+yusuke0127@users.noreply.github.com>
2023-02-02 15:55:10 +0900

    Refactor tests (#19)

--- spec/line_bot_spec.rb
+++ spec/line_bot_spec.rb
@@ -8,44 +8,32 @@ describe '#client' do
   end
 end
 
-describe '#is_keyword_in_message?' do
-  context 'Message with an accepted keyword' do
-    it 'returns true' do
-      expect(LineBot.is_keyword_in_message?(LineBot::ACCEPTED_KEYWORDS.keys, 'hi!')).to be true
-    end
+context 'Message with an accepted keyword' do
+  describe '#is_keyword_in_message?' do
+    it { expect(LineBot.is_keyword_in_message?(LineBot::ACCEPTED_KEYWORDS.keys, 'hi!')).to be true }
   end
 
-  context 'Message with an unaccepted keyword' do
-    it 'returns false' do
-      expect(LineBot.is_keyword_in_message?(LineBot::ACCEPTED_KEYWORDS.keys, 'random')).to be false
-    end
-  end
-end
-
-describe '#bot_reply_to' do
-  context 'Message with an accepted keyword' do
-    it 'returns true' do
-      expect(LineBot.bot_reply_to('hi!')).to include('Hello! Kamusta na 2mb mong brains?')
-    end
+  describe '#bot_reply_to' do
+    it { expect(LineBot.bot_reply_to('hi!')).to include('Hello! Kamusta na 2mb mong brains?') }
   end
 
-  context 'Message with an unaccepted keyword' do
-    it 'returns false' do
-      expect(LineBot.bot_reply_to('random')).to eq('')
-    end
-  end
-end
-
-describe '#get_keyword' do
-  context 'Message with an accepted keyword' do
+  describe '#get_keyword' do
     it 'returns the correct reply' do
       expect(LineBot.get_keyword(LineBot::ACCEPTED_KEYWORDS.keys, 'hi!')).to eq(:hi)
     end
   end
+end
 
-  context 'Message with an unaccepted keyword' do
-    it 'returns an empty string' do
-      expect(LineBot.get_keyword(LineBot::ACCEPTED_KEYWORDS.keys, 'random')).to eq(nil)
-    end
+context 'Message with an unaccepted keyword' do
+  describe '#is_keyword_in_message?' do
+    it { expect(LineBot.is_keyword_in_message?(LineBot::ACCEPTED_KEYWORDS.keys, 'random')).to be false }
+  end
+
+  describe '#bot_reply_to' do
+    it { expect(LineBot.bot_reply_to('random')).to eq('') }
+  end
+
+  describe '#get_keyword' do
+    it { expect(LineBot.get_keyword(LineBot::ACCEPTED_KEYWORDS.keys, 'random')).to be_nil }
   end
 end
